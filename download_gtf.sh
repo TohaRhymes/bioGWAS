04d5341133d4a5fd155d1357d9612d778276de04
TohaRhymes <anton@bioinf.me>
2023-04-28 20:39:44 +0000

    changed download gtf

--- download_gtf.sh
+++ download_gtf.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+DATA_DIR=$1
+
 # download gzip-file
 wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_37/gencode.v37.annotation.gtf.gz
 gzip -d gencode.v37.annotation.gtf.gz 
 
-mv gencode.v37.annotation.gtf data
+mv gencode.v37.annotation.gtf ${DATA_DIR}
 
