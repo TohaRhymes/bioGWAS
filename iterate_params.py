5c011d466e0f726a33a640a31ca078bf9333df4c
TohaRhymes <anton@bioinf.me>
2023-06-05 15:49:52 +0000

    results added

--- iterate_params.py
+++ iterate_params.py
@@ -22,16 +22,6 @@ sd_betas = (
     (0.01, 0.1, 0.3),
 )
 
-# try these m_betas
-m_betas = (
-    0.2, 
-    0.3)
-# use these sd for m_betas above respectively
-sd_betas = (
-    (0.1,),
-    (0.1,)
-)
-
 Ks = [
      10, 
      30, 
@@ -61,13 +51,14 @@ pprint(params)
 
 for K in Ks:
     k = K // 2
+    shared = 1.0
     for param in params:
         m_beta = param["m_beta"]
         sd_beta = param["sd_beta"]
         gen_var = param["gen_var"]
         h2s = param["h2s"]
         sim_id = f"m{m_beta}_sd{sd_beta}_gv{gen_var}_h2s{h2s}"
-        phenos_id = f"ph_sperm_K{K}"
+        phenos_id = f"ph_ss_K{K}"
         command = f"snakemake \
         --cores 8 \
         --config \
@@ -77,12 +68,13 @@ for K in Ks:
         h2s={h2s} \
         K={K} \
         k={k} \
-        draw_flag={True} \
+        shared={shared} \
+        draw_flag={False} \
         sim_id={sim_id} \
         phenos_id={phenos_id}"
         print(command)
         process = subprocess.Popen(
-            command, stdout=subprocess.PIPE, shell=True, executable="/bin/bash"
+             command, stdout=subprocess.PIPE, shell=True, executable="/bin/bash"
         )
         output, error = process.communicate()
         print("\n=============================\nOUTPUT: \n", output)
--- iterate_results.py
+++ iterate_results.py
@@ -32,16 +32,16 @@ def get_snps(line):
 # In[24]:
 
 
-CAUSAL_SNP_FILE = "data/chr_ph_sperm_K{}_chosen_snps.tsv"
-GWAS_FILE = "data/chr_ph_sperm_K{}_m{}_sd{}_gv{}_h2s{}_gwas.tsv"
-
-
+CAUSAL_SNP_FILE = "data2/chr_ph_ss_K{}_chosen_snps.tsv"
+GWAS_FILE = "data2/chr_ph_ss_K{}_m{}_sd{}_gv{}_h2s{}_gwas.tsv"
+FILE_RESULTS="data2/chr_ph_sperm_compare_results.tsv"
+BFILE="data2/chr_filt_sim"
 # ## Params for one K
 
 # In[4]:
 
 
-Ks = [10, 30, 50]
+Ks = [10, 30, 50 ]
 
 # try these m_betas
 m_betas = (
@@ -103,107 +103,109 @@ pprint(gv_h2s_comb)
 # In[166]:
 
 
-# result_set = []
+result_set = []
 
-# ## SNPs set for every k
-# for param in tqdm(params):
-#     for K in Ks:
-#         m = param["m_beta"]
-#         sd = param["sd_beta"]
-#         gv = param["gen_var"]
-#         h2s = param["h2s"]
+## SNPs set for every k
+for param in tqdm(params):
+    for K in Ks:
+        m = param["m_beta"]
+        sd = param["sd_beta"]
+        gv = param["gen_var"]
+        h2s = param["h2s"]
 
-#         causal_snp_file = CAUSAL_SNP_FILE.format(K)
-#         gwas_file = GWAS_FILE.format(K, m, sd, gv, h2s)
-#         # count number of snps
-#         with open(gwas_file, "rb") as f:
-#             N = sum(1 for _ in f) - 1
-#         # SET OF CAUSAL SNPS
-#         causal_snp = pd.read_csv(causal_snp_file, sep='\t', header=None, names=['chr', 'pos', 'pos_e', 'gene'])
-#         causal_set = set(causal_snp.chr.astype(str)+':'+causal_snp.pos.astype(str))
-#         print(causal_set)
+        causal_snp_file = CAUSAL_SNP_FILE.format(K)
+        gwas_file = GWAS_FILE.format(K, m, sd, gv, h2s)
+        # count number of snps
+        with open(gwas_file, "rb") as f:
+            N = sum(1 for _ in f) - 1
+        # SET OF CAUSAL SNPS
+        causal_snp = pd.read_csv(causal_snp_file, sep='\t', header=None, names=['chr', 'pos', 'pos_e', 'gene'])
+        causal_set = set(causal_snp.chr.astype(str)+':'+causal_snp.pos.astype(str))
+        print(causal_set)
 
-#         # make clump file
-#         FILE = gwas_file.replace('.tsv', '')
-#         BFILE = "data/chr_filt_sim"
-#         PVAL_CUTOFF = 0.05/N
-#         KB = 1000
+        # make clump file
+        FILE = gwas_file.replace('.tsv', '')
+        PVAL_CUTOFF = 0.05/N
+        KB = 1000
 
-#         command_format = f"""sed '1{{ s/chr/CHR/; s/rsid/SNP/; s/pos/BP/; s/n/NMISS/; s/beta/BETA/; s/se/SE/; s/r2/R2/; s/t/T/; s/pval/P/;}}' {FILE}.tsv | awk '{{$1=$1}};1'| tr -s ' ' '\t' > {FILE}.qassoc"""
-#         command_clump = f"""plink         --bfile {BFILE}         --allow-no-sex         --clump {FILE}.qassoc         --clump-p1 {'{:.30f}'.format(PVAL_CUTOFF)}         --clump-p2 {'{:.30f}'.format(PVAL_CUTOFF)}         --clump-r2 0.2         --clump-kb {KB}         --out {FILE}"""
-#         print(command_format)
-#         run(command_format)
-#         print(command_clump)
-#         run(command_clump)
-#         print('done clump')
+        try:
+            clumps_data = pd.read_fwf(f"{FILE}.clumped", sep='\t')
+        except FileNotFoundError:
+            command_format = f"""sed '1{{ s/chr/CHR/; s/rsid/SNP/; s/pos/BP/; s/n/NMISS/; s/beta/BETA/; s/se/SE/; s/r2/R2/; s/t/T/; s/pval/P/;}}' {FILE}.tsv | awk '{{$1=$1}};1'| tr -s ' ' '\t' > {FILE}.qassoc"""
+            command_clump = f"""plink         --bfile {BFILE}         --allow-no-sex         --clump {FILE}.qassoc         --clump-p1 {'{:.30f}'.format(PVAL_CUTOFF)}         --clump-p2 {'{:.30f}'.format(PVAL_CUTOFF)}         --clump-r2 0.2         --clump-kb {KB}         --out {FILE}"""
+            print(command_format)
+            run(command_format)
+            print(command_clump)
+            run(command_clump)
+            print('done clump')
 
-#         # read clump file and make list of snps for every clump
-#         try:
-#             clumps_data = pd.read_fwf(f"{FILE}.clumped", sep='\t')
-#         except FileNotFoundError:
-#             results = {"K": K, 
-#            "m_beta" : m, 
-#            "sd_beta" : sd, 
-#            "gen_var" : gv, 
-#            "h2s" : h2s,
-#             'clumps_total': 0,
-#             'clumps_causal': 0,
-#             'clumps_not_causal': 0,
-#             'causal_found': 0,
-#            'causal_not_found': 1,
-#           }
-#             result_set.append(results)
-#             continue
+            # read clump file and make list of snps for every clump
+            try:
+                clumps_data = pd.read_fwf(f"{FILE}.clumped", sep='\t')
+            except FileNotFoundError:
+                results = {"K": K, 
+               "m_beta" : m, 
+               "sd_beta" : sd, 
+               "gen_var" : gv, 
+               "h2s" : h2s,
+                'clumps_total': 0,
+                'clumps_causal': 0,
+                'clumps_not_causal': 0,
+                'causal_found': 0,
+               'causal_not_found': 1,
+              }
+                result_set.append(results)
+                continue
             
-#         clumps_data['SP2_list'] = clumps_data.SP2.apply(get_snps)
-#         clumps_data.sort_values(by=["CHR", "BP"],
-#                        inplace = True,)
-#         clumps_data.head(20)
-#         snp2list = defaultdict(list)
-#         last_pos = -1
-#         last_chrom = -1
-#         last_SNP = -1
-#         for key, value in clumps_data.iterrows():
-#             if last_chrom != value.CHR or (last_chrom == value.CHR and abs(last_pos - value.BP) > KB * 1000):
-#                 if last_SNP != -1:
-#                     snp2list[last_SNP] = set(flatten(snp2list[last_SNP]))
-#                 last_SNP = value.SNP
-#             last_chrom = value.CHR
-#             last_pos = value.BP
-#             snp2list[last_SNP].append(value.SP2_list+[value.SNP])
-#         snp2list[last_SNP] = set(flatten(snp2list[last_SNP]))
-#         snp2list = dict(snp2list)
+        clumps_data['SP2_list'] = clumps_data.SP2.apply(get_snps)
+        clumps_data.sort_values(by=["CHR", "BP"],
+                       inplace = True,)
 
-#         # make dict with results for this paramset
-#         clumps_causal = []
-#         causal_found = []
-#         for snp in causal_set:
-#             for key in snp2list:
-#                 if snp in snp2list[key]:
-#                     clumps_causal.append(key)
-#                     causal_found.append(snp)
-#                     break
-#         clumps_not_causal = set(snp2list.keys()) - set(clumps_causal)
-#         causal_not_found = causal_set - set(causal_found)
+        snp2list = defaultdict(list)
+        last_pos = -1
+        last_chrom = -1
+        last_SNP = -1
+        for key, value in clumps_data.iterrows():
+            if last_chrom != value.CHR or (last_chrom == value.CHR and abs(last_pos - value.BP) > KB * 1000):
+                if last_SNP != -1:
+                    snp2list[last_SNP] = set(flatten(snp2list[last_SNP]))
+                last_SNP = value.SNP
+            last_chrom = value.CHR
+            last_pos = value.BP
+            snp2list[last_SNP].append(value.SP2_list+[value.SNP])
+        snp2list[last_SNP] = set(flatten(snp2list[last_SNP]))
+        snp2list = dict(snp2list)
 
-
-#         results = {"K": K, 
-#                    "m_beta" : m, 
-#                    "sd_beta" : sd, 
-#                    "gen_var" : gv, 
-#                    "h2s" : h2s,
-#                     'clumps_total': len(snp2list),
-#                     'clumps_causal': round(len(clumps_causal)/len(snp2list), 5),
-#                     'clumps_not_causal': round(len(clumps_not_causal)/len(snp2list), 5),
-#                     'causal_found': round(len(causal_found)/K, 5),
-#                    'causal_not_found': round(len(causal_not_found)/K, 5),
-#                   }
-
-#         result_set.append(results)
+        # make dict with results for this paramset
+        clumps_causal = []
+        causal_found = []
+        for snp in causal_set:
+            for key in snp2list:
+                if snp in snp2list[key]:
+                    clumps_causal.append(key)
+                    causal_found.append(snp)
+                    break
+        clumps_not_causal = set(snp2list.keys()) - set(clumps_causal)
+        causal_not_found = causal_set - set(causal_found)
 
 
-# # In[167]:
+        results = {"K": K, 
+                   "m_beta" : m, 
+                   "sd_beta" : sd, 
+                   "gen_var" : gv, 
+                   "h2s" : h2s,
+                    'clumps_total': len(snp2list),
+                    'clumps_causal': round(len(clumps_causal)/len(snp2list), 5),
+                    'clumps_not_causal': round(len(clumps_not_causal)/len(snp2list), 5),
+                    'causal_found': round(len(causal_found)/K, 5),
+                   'causal_not_found': round(len(causal_not_found)/K, 5),
+                  }
+
+        result_set.append(results)
 
 
-# result_table = pd.DataFrame(result_set)
-# result_table.to_csv("data/chr_ph_sperm_compare_results.tsv", sep='\t', index=False)
+# In[167]:
+
+
+result_table = pd.DataFrame(result_set)
+result_table.to_csv(FILE_RESULTS, sep='\t', index=False)
